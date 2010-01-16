f29011d242329fc502ea6df1345d4ee69f9ffdae
Ethan Shepherd <someguy235@gmail.com>
2010-01-16 13:32:45 -0500

    stage 1

--- spoj02.groovy
+++ spoj02.groovy
@@ -1,4 +1,13 @@
 
-class spoj02 {
+class spoj02Class {}
 
+System.in.withReader{
+	print '# of test cases:'
+	t = it.readLine()
+	println t.class
+	//println t.intValue()
+	//println Integer.valueOf(t)
+	for (i in '1'..t){
+		println i
+	}
 }
