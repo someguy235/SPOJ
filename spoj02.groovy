ac6a45dd7e3f7e56a30948a513abf7ba8aa53164
Ethan Shepherd <someguy235@gmail.com>
2010-01-17 13:40:56 -0500

    ingest done

--- spoj02.groovy
+++ spoj02.groovy
@@ -1,13 +1,26 @@
 
 class spoj02Class {}
 
+def ranges = []
 System.in.withReader{
 	print '# of test cases:'
-	t = it.readLine()
-	println t.class
-	//println t.intValue()
-	//println Integer.valueOf(t)
-	for (i in '1'..t){
-		println i
+	t = Integer.parseInt(it.readLine())
+	for (i in 1..t){
+		print 'enter min['+i+'] max['+i+'] (ex. "1 2") : '
+		line = it.readLine()	
+		range = line.split(' ')
+		ranges.add(range)
+	}
+	for (i in 0..t-1){
+		println i+1 + ': ' + ranges[i][0] + ', ' + ranges[i][1]
+		sieve(Integer.parseInt(ranges[i][0]), Integer.parseInt(ranges[i][1]))
 	}
 }
+def sieve(min, max){
+	primes = []
+	for (i in 1..max){
+		primes[i] = true
+	}
+	
+}
+println "done"
